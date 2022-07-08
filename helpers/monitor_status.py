454fad5b63222c52d1a3f6f5c5de6d564957a1fa
Euchupi <youngroosevelt@163.com>
2022-07-09 00:57:31 +0800

    Modify the monitor pthon script

--- helpers/monitor_status.py
+++ helpers/monitor_status.py
@@ -3,19 +3,17 @@ from pymongo import MongoClient
 import os
 import time
 
-client = MongoClient("mongodb://daq:%s@xenon1t-daq:27017/admin"%os.environ["MONGO_PASSWORD"])
+client = MongoClient("mongodb://192.168.1.88:27017/admin")
 
+# This python script keep reading the status of the detector 
 db = client['daq']
 collection = db['status']
 
-clients = ["fdaq00"]
 STATUS = ["Idle", "Arming", "Armed", "Running", "Error"]
 
+
 while 1:
-    for c in clients:
-        docs= collection.find({"host": c}).sort("_id", -1).limit(1)
-        doc = list(docs)[0]
-        print("%s: Client %s reports status: %s rate: %.2f buffer: %i"%(doc['_id'].generation_time,
-                                                                        doc['host'], STATUS[doc['status']], doc['rate'],
-                                                                        doc['buffer_length']))
+    docs= collection.find({}).sort("_id", -1).limit(1)
+    for doc in docs:
+        print("%s: Client %s reports status: %s rate: %.2f: %i"%(doc['_id'].generation_time, doc['host'], STATUS[doc['status']], doc['rate'],doc['buffer_length']))
     time.sleep(1)
