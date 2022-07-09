7539384e1bb8adeb4963486e31f3ac4fba3bab01
Euchupi <youngroosevelt@163.com>
2022-07-09 08:37:31 +0800

    run_command_moodify_again

--- helpers/runcommand.py
+++ helpers/runcommand.py
@@ -5,19 +5,22 @@ import datetime
 import glob
 
 
+# information we need :
+#  --command : 
+#  --number : to log the number of the run 
+#  --mode : to log the set_run_mode 
+#  --host: Which host set the command 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--command', choices='check write arm stop start'.split(), required=True, help='The command')
     parser.add_argument('--number', type=int, default=0, help='Run number')
     parser.add_argument('--mode', help='Run mode', required=True)
-    parser.add_argument('--host', nargs='+', default='muttley_reader_0', help="Hosts to issue to")
+    parser.add_argument('--host', nargs='+', default=[os.uname()[1]], help="Hosts to issue to")
 
     args = parser.parse_args()
-
     return args
 
 def insert_control(coll, args):
-
     doc = {
         "command": args.command,
         "number": args.number,
@@ -29,26 +32,21 @@ def insert_control(coll, args):
         "createdAt": datetime.datetime.now()
     }
     coll.insert_one(doc)
-
     return
 
 
 def check_repeated(path, index):
     existing_files = glob.glob(path + '/*')
     current_file_name = path + '/' + str(0) * (6 - len(str(index))) + str(index)
-
     while current_file_name in existing_files:
         index += 1
         current_file_name = path + '/' + str(0) * (6 - len(str(index))) + str(index)
-
     return index
 
 if __name__ == '__main__':
 
     with MongoClient("mongodb://192.168.1.88:27017") as client:
-
         args=main()
-
         path = client['daq']['options'].find_one({'name':args.mode})['strax_output_path']
         index = args.number
         new_index = check_repeated(path, index)
