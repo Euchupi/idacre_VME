7280ee50f6c77b497a4f142b0469457af35ac53f
Euchupi <youngroosevelt@163.com>
2022-07-09 15:23:13 +0800

    monitor_time_output

--- helpers/monitor_status.py
+++ helpers/monitor_status.py
@@ -15,4 +15,6 @@ while 1:
     docs= collection.find({}).sort("_id", -1).limit(1)
     for doc in docs:
         print("%s: Client %s reports status: %s rate: %.2f: %i"%(doc['_id'].generation_time, doc['host'], STATUS[doc['status']], doc['rate'],doc['buffer_length']))
+    
+    print(time.time())
     time.sleep(1)
