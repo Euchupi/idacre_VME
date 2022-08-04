60008a34743e16db20903e5887c07ab6c6af2214
Euchupi <youngroosevelt@163.com>
2022-08-05 02:37:25 +0800

    renew set_mode

--- helpers/run_modes/set_mode_V1725.py
+++ helpers/run_modes/set_mode_V1725.py
@@ -32,10 +32,11 @@ run_mode = {
     "strax_chunk_length": 5000000000,
     "strax_fragment_length": 220,
     "baseline_dac_mode": "fit",
-	"baseline_fallback_mode": "cached" , 
+	"baseline_dac_mode.tpc": "fit"
+	"baseline_fallback_mode": "fit" , 
     "baseline_value": 16000,
-	"baseline_reference_run": 1 ,
-	"baseline_reference_run_tpc": 1 , 
+	"baseline_reference_run": -1 ,
+	"baseline_reference_run.tpc": -1 , 
     "firmware_version": 4.22,
     "boards":
     [
