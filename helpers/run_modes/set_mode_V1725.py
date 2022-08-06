b78a56e610f4f309555e70e98321f77885e1a796
Euchupi <youngroosevelt@163.com>
2022-08-06 21:33:31 +0800

    Hope there would be some data

--- helpers/run_modes/set_mode_V1725.py
+++ helpers/run_modes/set_mode_V1725.py
@@ -75,12 +75,84 @@ run_mode = {
             "vme_address": "0", "type": "V1730", "host": "RelicsDAQ_reader_0"},
     ],
     "registers" : [
-		{
-			"comment" : "board reset register",
-			"board" : -1,
-			"reg" : "EF24",
-			"val" : "0"
-		},
+			{
+					"comment" : "board reset register",
+					"board" : 'all',
+					"reg" : "EF24",
+					"val" : "0"
+			},
+			{
+					"comment" : "events per BLT-originally it was 1",
+					"board" : 'all',
+					"reg" : "EF1C",
+					"val" : "1"
+			},
+			{
+					"comment" : "Front Panel Trigger Out Enable Mask",
+					"board" : 'all',
+					"reg" : "8110",
+					"val" : "00000000"
+			},
+			{
+					"comment" : "for  BUSY on TRG_OUT, LVDS new management, General Purpose I/O, LVDS[15-4] outputs, LVDS[3-0] inputs, TRG/CLK at TTL level (139) or NIM level (138)",
+					"board" : 'all',
+					"reg" : "811C",
+					"val" : "D0138"
+			},
+			{
+					"comment" : "BERR register, 10=enable BERR",
+					"board" : 'all',
+					"reg" : "EF00",
+					"val" : "10"
+			},
+			{
+					"comment" : "Trigger logic: 80000000 is software only, software + external C0000000",
+					"board" : 'all',
+					"reg" : "810C",
+					"val" : "C0000000"
+			},
+			{
+					"comment" : "Post Trigger (time between trigger and end of time window). 80 for 2.5 us",
+					"board" : 'all',
+					"reg" : "8114",
+					"val" : time_to_hex(5000)
+			},
+			{
+				"comment": "Channel enable mask. FF= all channels on",
+				"board": 945,
+				"reg": "8120",
+				"val": "0"
+			},
+			{
+				"comment": "Channel enable mask. FF= all channels on",
+				"board": 971,
+				"reg": "8120",
+				"val": "F"
+			},
+			{
+					"comment" : "Buffer organization register. A for new FW.",
+					"board" : 'all',
+					"reg" : "800C",
+					"val" : "7"
+			},
+			{
+					"comment" : "DAC default configuration. 1000 = neg unipolar + offset, 1000",
+					"board" : 'all',
+					"reg" : "8098",
+					"val" : "1000"
+			},
+			{
+					"comment" : "50: NO ZS + Falling + Sequential + External signal + Non Overlap Triggers, 20050 same with ZLE",
+					"board" : 'all',
+					"reg" : "8000",
+					"val" : "50"
+			},
+			{
+					"comment" : "Event size register. Required for new FW. Words.",
+					"board" : 'all',
+					"reg" : "8020",
+					"val" : time_to_hex(10000)
+			},
 	],
 	
 	"thresholds":{"0":[0,0,0,0,0,0,0,0]},
